cf1811b345a00b537d8bec017a2c64409ba73499
Boboye <boboye771@gmail.com>
2024-02-06 19:06:33 +0100

    my first commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import useSound from "use-sound"
+import './App.css';
+import NoButton from './components/NoButton/NoButton';
+import YesButton from './components/YesButton/YesButton';
+import Accepted from './components/Accepted/Accepted';
+import marriedLifeClip from "./married-life-clipped.mp3"
+
+import { motion } from "framer-motion"
+
+function App() {
+  const [isAccepted, setIsAccepted] = useState(false)
+  const [playSound, { stop }] = useSound(marriedLifeClip);
+
+
+
+
+  return (
+    <motion.div className="app" initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+      {
+        <>
+          <h1 className='header'>ARE YOU LOVED?(Try to say no)? &#129402;</h1>
+          {!isAccepted ? <div className='choice'>
+            <YesButton setIsAccepted={setIsAccepted} playSound={playSound} stopSound={stop} />
+            <NoButton />
+          </div> : <>
+            <div className='choice'>
+              <Accepted setIsAccepted={setIsAccepted} playSound={playSound} stopSound={stop} />
+            </div>
+          </>}
+        </>
+      }
+    </motion.div>
+  );
+}
+
+export default App;
---
+++ src/components/Accepted/Accepted.js
@@ -0,0 +1,19 @@
+import "./Accepted.css"
+import { Icon } from '@iconify/react';
+import { motion } from "framer-motion"
+import Typewriter from 'typewriter-effect';
+const Accepted = ({ setIsAccepted, playSound, stopSound }) => {
+    return <motion.div className="accepted" initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+        <div className="close-icon" onClick={() => {
+            setIsAccepted(false)
+            stopSound()
+        }}><Icon icon="mi:close" /></div>
+        <div className="text"> <Typewriter onInit={(typewriter) => {
+            typewriter.typeString("You bet your fine ass he does.").pauseFor(3000).deleteAll().typeString("Don't ever forget it.").pauseFor(3000).deleteAll().typeString("You are Loved... <3 :)").start()
+        }} /></div>
+
+    </motion.div>
+}
+
+
+export default Accepted
